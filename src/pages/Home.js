c5c35b2ebb14b301c131792404d26fceb7fd0bb5
Dipit Sharma <superstark02@gmail.com>
2023-10-14 20:10:00 +0530

    feature added back

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,16 @@ import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import "../style/Home.css";
 import { useNavigate } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
-import {db} from "../firebase"
+import { db } from "../firebase";
 import Graphql from "../functions/grphql";
 
 function Home() {
-
   const [userLoggedIn, setUserLoggedin] = useState("Please wait...");
   const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
   const [graphqlData, setGraphqlData] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, async (user) => {
       if (user) {
@@ -27,76 +26,61 @@ function Home() {
         } else {
           console.log("No such document!");
         }
-      }
-      else{
+      } else {
         setUserLoggedin("Please Login/Signup");
       }
     });
-
-    const MY_QUERY = `
-    query {
-        data(limit:10){
-            _id,
-            name,
-            imageUrl
-        }
-    }`;
-
-    Graphql(MY_QUERY).then(res=>{
-      res.json().then(data=>{
-        setGraphqlData(data.data)
-      });
-    })
-  })
+  });
 
   const handleLogout = () => {
     const auth = getAuth();
-    signOut(auth).then(() => {
-      navigate("/login")
-    }).catch((error) => {
-      console.log(error)
-    });
-  }
+    signOut(auth)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
       <div className="welcome-parent">
         <div>
           <div className="welcome">Welcome! {userLoggedIn}</div>
-          {
-            userLoggedIn && userData ? (
-              <div className="text-center mt-10" >
-                <div>
-                  Name: {userData.name}
-                  <div className="flex justify-center" >
-                    <img alt="pp" className="profile-photo" src={userData.image_url}/>
-                  </div>
+          {userLoggedIn && userData ? (
+            <div className="text-center mt-10">
+              <div>
+                Name: {userData.name}
+                <div className="flex justify-center">
+                  <img
+                    alt="pp"
+                    className="profile-photo"
+                    src={userData.image_url}
+                  />
                 </div>
-                <div className="mt-20"  onClick={handleLogout}>Logout</div>
               </div>
-            ):(
-              <div></div>
-            )
-          }
+              <div className="mt-20" onClick={handleLogout}>
+                Logout
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
         <div>
-          {
-            graphqlData && graphqlData.map(item=>{
-              return(
+          {graphqlData &&
+            graphqlData.map((item) => {
+              return (
                 <div>
-                  <div>
-                    {item.name}
-                  </div>
+                  <div>{item.name}</div>
                   <div>
                     <img src={item.imageUrl} alt="char" />
                   </div>
-                  <div>
-                    {item.id}
-                  </div>
+                  <div>{item.id}</div>
                 </div>
-              )
-            })
-          }
+              );
+            })}
         </div>
       </div>
     </div>
